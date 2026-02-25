7ebb0fc23cf4cb750ca33d938eed7d0423e71884
Sebastian <sebastianignaciosantillan@gmail.com>
2026-02-25 01:26:22 -0300

    se añaden datos a la tabla de vehiculos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO vehiculos (marca, modelo, ano, color, tipo_combustible) VALUES
+('Toyota', 'Corolla', 2020, 'Blanco', 'Gasolina'),
+('Chevrolet', 'Spark', 2019, 'Rojo', 'Gasolina'),
+('Ford', 'Ranger', 2021, 'Gris', 'Diesel'),
+('Honda', 'Civic', 2022, 'Negro', 'Gasolina'),
+('Nissan', 'Frontier', 2020, 'Azul', 'Diesel');
